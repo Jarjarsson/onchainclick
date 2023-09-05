717f6fc43b1fdd72addcb76091efc61882a215af
Allan Heremi <allanheremidev@gmail.com>
2023-09-05 11:00:23 +0200

    Chore: test push

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. 
-
-          Test
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Developoors are here!
         </a>
       </header>
     </div>
