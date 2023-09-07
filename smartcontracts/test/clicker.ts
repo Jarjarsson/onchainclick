d58189e1a88d97dd23c7fe55f11bda801d5894a5
rofun <joufang.w@gmail.com>
2023-09-07 15:59:54 +0800

    Feat: finished unit tests

--- smartcontracts/test/clicker.ts
+++ smartcontracts/test/clicker.ts
@@ -1,13 +1,13 @@
-import {expect} from 'chai';
-import { ethers } from 'hardhat';
+import { expect } from "chai";
+import { ethers } from "hardhat";
 
-describe('Clicker Contract', function () {
+describe("Clicker Contract", function () {
   let clicker: any;
   let owner;
 
   // Before each test, deploy a new Clicker contract
   beforeEach(async function () {
-    const Clicker = await ethers.getContractFactory('Clicker');
+    const Clicker = await ethers.getContractFactory("Clicker");
     clicker = await Clicker.deploy();
     await clicker.waitForDeployment();
 
@@ -15,28 +15,30 @@ describe('Clicker Contract', function () {
     [owner] = await ethers.getSigners();
   });
 
-  it('Should deploy the contract with an initial count of 0', async function () {
+  it("Should deploy the contract with an initial count of 0", async function () {
     const count = await clicker.clickedCount();
     expect(count).to.equal(0);
   });
 
-  it('Should increment the count when calling the click function', async function () {
+  it("Should increment the count when calling the click function", async function () {
     await clicker.click();
     const count = await clicker.clickedCount();
     expect(count).to.equal(1);
   });
 
-  it('Should reset the count when calling the reset function', async function () {
+  it("Should reset the count when calling the reset function", async function () {
     await clicker.click();
     await clicker.reset();
     const count = await clicker.clickedCount();
     expect(count).to.equal(0);
   });
 
-  it('Should emit a Count event when calling the click function', async function () {
-    const tx = await clicker.click();
-    const receipt = await tx.wait();
-    expect(receipt.events[0].event).to.equal('Count');
-    expect(receipt.events[0].args[0]).to.equal(1);
+  it("Should emit a Count event when calling the click function", async function () {
+    await expect(clicker.click()).to.emit(clicker, "Count");
+    // const tx = await clicker.click();
+    // const receipt = await tx.wait();
+    // console.log(receipt);
+    // expect(receipt.events[0].event).to.equal("Count");
+    // expect(receipt.events[0].args[0]).to.equal(1);
   });
 });
