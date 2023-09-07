a01b73f7dc606005da52f87908644a13388f1cb2
rofun <joufang.w@gmail.com>
2023-09-07 17:08:05 +0800

    Feat: deployed smart contract locally

--- smartcontracts/hardhat.config.ts
+++ smartcontracts/hardhat.config.ts
@@ -2,8 +2,20 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
 
+const ALCHEMY_API_KEY = "do2Q1a5e3JFBvrkk1WOH4HAK3XvVpczJ";
+const GOERLI_PRIVATE_KEY =
+  "ba1cabb173d1e0d386c03e0e6fd52fcd914bdec9fd3142ac310471b93621c667";
+const API_URL =
+  "https://eth-goerli.g.alchemy.com/v2/do2Q1a5e3JFBvrkk1WOH4HAK3XvVpczJ";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    goerli: {
+      url: API_URL,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
